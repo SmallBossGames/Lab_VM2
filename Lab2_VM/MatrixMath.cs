5ff7b1f5d04a54b5d98282e3ce6a80046e151069
Алексей Гардер <aleksey_garder@outlook.com>
2018-03-22 21:56:19 +0700

    Добавьте файлы проекта.

---
+++ Lab2_VM/MatrixMath.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2_VM
+{
+    public class MatrixMath
+    {
+
+        public static decimal[] CalculateGauss(decimal[,] matrix)
+        {
+            var strCount = matrix.GetLength(0);
+            var columnCount = matrix.GetLength(1);
+            var resArray = new decimal[strCount];
+
+            if (strCount + 1 != columnCount) throw new IncorrectMatrixException();
+
+            for (var i = 0; i < strCount; i++)
+            {
+                var maxIndex = MaxAbsStringIndex(matrix, i, i, strCount-1);
+
+                if (matrix[i, maxIndex] == 0) throw new IncorrectMatrixException();
+
+                SwapString(matrix, i, maxIndex);
+
+                for (var j = i + 1; j < strCount; j++)
+                {
+                    var koeff = -(matrix[j, i] / matrix[i, i]);
+                    StringSum(matrix, i, j, koeff);
+                    matrix[j, i] = 0;
+                }
+
+                DivideString(matrix, i, matrix[i, i]);
+            }
+
+            for (var i = strCount - 1; i > -1; i--) 
+            {
+                var result = matrix[i, columnCount - 1];
+
+                for (var j = i + 1; j < strCount; j++)
+                    result -= matrix[i, j] * resArray[j];
+
+                resArray[i] = result;
+            }
+
+            for (var i = 0 ; i < strCount; i++)
+            {
+                for (var j = 0; j < columnCount; j++)
+                    Console.Write(matrix[i, j].ToString() + ' ');
+                Console.WriteLine();
+            }
+
+            return resArray;
+        }
+
+        public static int MaxAbsStringIndex(decimal[,] matrix, int column, int startStr, int endStr)
+        {
+            int maxIndex = startStr;
+            for (int i = startStr; i <= endStr; i++)
+            {
+                if (Abs(matrix[maxIndex, column]) < Abs(matrix[i, column]))
+                    maxIndex = i;
+            }
+            return maxIndex;
+        }
+
+        static void SwapString(decimal[,] matrix, int index1, int index2)
+        {
+            if (index1 == index2) return;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                var pool = matrix[index1, i];
+                matrix[index1, i] = matrix[index2, i];
+                matrix[index2, i] = pool;
+            }
+        }
+
+        static void StringSum(decimal[,] matrix, int from, int to, decimal koeff)
+        {
+            for (int i = 0; i < matrix.GetLength(1); i++)
+                matrix[to, i] += matrix[from, i] * koeff;
+        }
+
+        static void DivideString(decimal[,] matrix, int index, decimal koeff)
+        {
+            var length = matrix.GetLength(1);
+            for (var i = 0; i < length; i++)
+                matrix[index, i] /= koeff;
+        }
+
+        public static decimal Abs(decimal value) => value > 0 ? value : -value;
+
+        class IncorrectMatrixException:Exception
+        {
+            public override string Message => "Matrix is incorrect";
+        }
+    }
+}
---
+++ UnitTestLab2/UnitTest1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab2_VM;
+
+namespace UnitTestLab2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        
+
+        [TestMethod]
+        public void TestMaxStringIndex()
+        {
+            decimal[,] matrix =
+            {
+                { 1,2,3,4,5 },
+                { 4,6,1,8,2 },
+                { 6,228,14,-3,-6 },
+                { -99,6,1,8,2 },
+
+            };
+
+            var validData = 3;
+            var testData = MatrixMath.MaxAbsStringIndex(matrix, 0, 0, matrix.GetLength(0) - 1);
+            Assert.AreEqual(testData, validData, 0, "Wrong value");
+
+            validData = 3;
+            testData= MatrixMath.MaxAbsStringIndex(matrix, 3, matrix.GetLength(0) - 1, matrix.GetLength(0) - 1);
+            Assert.AreEqual(testData, validData, 0, "Wrong value");
+        }
+
+        [TestMethod]
+        public void TestGauss1()
+        {
+            decimal[,] matrix =
+            {
+                { 1,2,3,3 },
+                { 3,5,7,0 },
+                { 1,3,4,1 },
+            };
+
+            decimal[] validData = { -4, -13, 11 };
+            decimal[] data = MatrixMath.CalculateGauss(matrix);
+            decimal delta = 0.0001m;
+
+            Assert.AreEqual(validData.Length, data.Length);
+
+            for (var i = 0; i < validData.Length; i++)
+                if (MatrixMath.Abs(data[i] - validData[i]) > delta)
+                    Assert.Fail(MatrixMath.Abs(data[i] - validData[i]).ToString());
+        }
+
+        [TestMethod]
+        public void TestGauss2()
+        {
+            decimal[,] matrix =
+            {
+                { -7,2,-2,-7 },
+                { -2,5,-2,5 },
+                { -3,-1,-5,-5 },
+            };
+
+            decimal[] validData = { -15.75m, -1.375m, -1 };
+            decimal[] data = MatrixMath.CalculateGauss(matrix);
+            decimal delta = 0.01m;
+
+            Assert.AreEqual(validData.Length, data.Length);
+
+            for (var i = 0; i < validData.Length; i++)
+                if (MatrixMath.Abs(data[i] - validData[i]) > delta)
+                    Assert.Fail(MatrixMath.Abs(data[i] - validData[i]).ToString());
+        }
+    }
+}
